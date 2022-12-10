5a0b005091a9db0bc508572e212d351c76e44334
Dennis Schwartz <dennis.schwartz@benevolent.ai>
2022-12-10 21:58:14 +0000

    Added day7

---
+++ day7/part2.py
@@ -0,0 +1,100 @@
+import copy
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import Dict, List, Union
+
+
+@dataclass
+class File:
+    name: str
+    size: int
+
+
+@dataclass
+class Directory:
+    parent_dir: 'Directory'
+    name: str
+    abs_path: List[str] = field(default_factory=list)
+    contents: List = field(default_factory=dict)
+
+
+def du(obj: Union[Directory, File]) -> int:
+    if isinstance(obj, File):
+        return obj.size
+    if isinstance(obj, Directory):
+        size = sum([du(c) for c in obj.contents.values()])
+        print('/' + f'{"/".join(obj.abs_path)} ({size})')
+        return size
+    raise TypeError('Only Directory or File are supported')
+
+
+def get_sizes(cwd: Directory, acc: Dict[str, int] = defaultdict(int)) -> Dict[str, int]:
+    assert isinstance(cwd, Directory)
+    acc[f'/{"/".join(cwd.abs_path)}'] = du(cwd)
+    for c in cwd.contents.values():
+        if isinstance(c, Directory):
+            acc = get_sizes(c, acc)
+    return acc
+
+
+def tree(obj: Union[Directory, File], depth: int = 0) -> None:
+    padding = '  ' * depth
+    if isinstance(obj, Directory):
+        print(f'{padding}- {obj.name} (dir)')
+        for c in obj.contents.values():
+            tree(c, depth=depth+1)
+    if isinstance(obj, File):
+        print(f'{padding}- {obj.name} (file, size={obj.size})')
+
+
+def main(in_file):
+    lines = in_file.readlines()
+    cwd: Directory = Directory(name='root', parent_dir=None)
+
+    for line in lines:
+        if line.startswith("$"):
+            tmp = line.split()
+            cmd = tmp[1]
+            if cmd == 'ls':
+                continue
+            if cmd == 'cd':
+                if tmp[2] == '..':
+                    cwd = cwd.parent_dir
+                elif tmp[2] == '/':
+                    while cwd.parent_dir is not None:
+                        cwd = cwd.parent_dir
+                else:
+                    cwd = cwd.contents[tmp[2]]
+        else:
+            tmp = line.split()
+            if tmp[0] == 'dir':
+                cwd.contents[tmp[1]] = Directory(
+                    name=tmp[1],
+                    parent_dir=cwd,
+                    abs_path=cwd.abs_path + [tmp[1]]
+                )
+            else:
+                cwd.contents[tmp[1]] = File(name=tmp[1], size=int(tmp[0]))
+
+    # Move to root
+    while cwd.parent_dir is not None:
+        cwd = cwd.parent_dir
+
+    # tree(cwd)
+
+    dir_sizes = get_sizes(cwd)
+    disk_size = 70000000
+    used = dir_sizes['/']
+    print(f'Used: {used} of {disk_size}. Free space: {disk_size-used}')
+    needed = 30000000
+    to_delete = needed - (disk_size - used)
+    print(f'To delete: {to_delete}')
+    t = [(s, n) for n, s in dir_sizes.items() if s >= 389918]
+    t = sorted(t, key=lambda x: x[0])
+    return t.pop(0)[0]
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        size = main(f)
+        print(size)
